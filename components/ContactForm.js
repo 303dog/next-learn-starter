f35b78def696a0691eeeac77d3df96cbb5d61f4a
Paul_Beckwith <303dog@gmail.com>
2021-04-17 14:20:02 -0600

    test form render

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,32 +1,21 @@
-
-
-
 export default function ContactForm() {
-    return (
-      <form name="contact" action='/success' method="POST" data-netlify='true'>
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-  <input type="text" name="firstname" id="firstname" />
-          <label htmlFor="yourname">
-            Your Name:
-          </label> <br />
-          <input type="text" name="name" id="yourname" />
-        </p>
-        <p>
-          <label htmlFor="youremail">
-            Your Email:
-          </label> <br />
-          <input type="email" name="email" id="youremail" />
-        </p>
-        <p>
-          <label htmlFor="yourmessage">
-            Message:
-          </label> <br />
-          <textarea name="message" id="yourmessage"></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    )
-  }
+  return (
+    <form name='contact' method='POST' data-netlify='true'>
+      <p>
+        <label for='name'>Name</label>
+        <input type='text' id='name' name='name' />
+      </p>
+      <p>
+        <label for='email'>Email</label>
+        <input type='text' id='email' name='email' />
+      </p>
+      <p>
+        <label for='message'>Message</label>
+        <textarea id='message' name='message'></textarea>
+      </p>
+      <p>
+        <button type='submit'>Send</button>
+      </p>
+    </form>
+  );
+}
