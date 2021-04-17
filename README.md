af9bc04ab14cc6f7ef9a1396a14699acbb541551
Paul_Beckwith <303dog@gmail.com>
2021-04-17 14:09:24 -0600

    refactor form

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ npm run dev
 yarn dev
 ```
 
+
+I just love **bold text**
+
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
--- pages/posts/requestForm.js
+++ pages/posts/requestForm.js
@@ -2,29 +2,27 @@ import Head from "next/head";
 import Layout from "../../components/layout";
 
 function RequestForm() {
-  const registerUser = async event => {
+  const registerUser = async (event) => {
     event.preventDefault();
 
-    const res = await fetch(
-        '/api/register',
-        {
-          body: JSON.stringify({
-            name: event.target.name.value,
-            email: event.target.email.value,
-            phnum: event.target.phnum.value,
+    const res = await fetch("/api/register", {
+      body: JSON.stringify({
+        name: event.target.name.value,
+        email: event.target.email.value,
+        // phnum: event.target.phnum.value,
+        message: event.target.message.value,
 
-            file: event.target.file.value,
-            submit: event.target.reset()
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          method: 'POST'
-        }
-      )
-  
-      const result = await res.json()
-      // result.user => 'Ada Lovelace'
+        upload: event.target.file.value,
+        submit: event.target.reset(),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    const result = await res.json();
+    // result.user => 'Ada Lovelace'
   };
 
   return (
@@ -33,19 +31,29 @@ function RequestForm() {
         <title>Form</title>
       </Head>
       <form onSubmit={registerUser}>
-        <label htmlFor='name'>Name</label>
+        <label htmlFor='name'>Name </label>
         <input id='name' name='name' type='text' autoComplete='name' required />
-
         <label htmlFor='email'>Email</label>
-        <input id='email' email='email' type='email' autoComplete='email' required />
-
+        <input
+          id='email'
+          name='email'
+          type='email'
+          autoComplete='email'
+          required
+        />
         <label htmlFor='textarea'>How can we help?</label>
-        <textarea id='registerUser' name='textarea' rows="4" cols="50" required />
-
+        <textarea
+          id='registerUser'
+          name='message'
+          rows='4'
+          cols='60'
+          required
+        />
         <label htmlFor='upload'>Upload a file</label>
-        <input id='file' type="file" />
-
-        <button type='submit' submit='submit'>Submit</button>
+        <input id='file' name='upload' type='file' />
+        <button type='submit' submit='submit'>
+          Submit
+        </button>
       </form>
     </Layout>
   );
