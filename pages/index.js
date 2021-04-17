e7325b13ee49f163a7f2977f6c2c3fdeba9298a9
Paul_Beckwith <303dog@gmail.com>
2021-04-16 21:22:39 -0600

    testing form structures

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>irononcarpetlabels.com</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Welcome</p>
--- pages/success.js
+++ pages/success.js
@@ -1,5 +1,17 @@
-
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from '../styles/utils.module.css'
 
 export default function Success() {
-    return <div>Form successfully submitted!</div>
-  }
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.card}>
+        <div className={utilStyles.code}>Form successfully submitted!</div>
+        <h2 className={utilStyles.code}>We will be in contact soon</h2>
+      </section>
+    </Layout>
+  );
+}
